7644d167e0610f837de2196112a660099221bdd1
bill <vassileiosmarkos@gmail.com>
2025-12-30 12:21:44 +0200

    Failed scalpel approach

--- preprocessor/src/Utils.hs
+++ preprocessor/src/Utils.hs
@@ -2,11 +2,30 @@
 
 module Utils where
 
-import          Data.Text
-import          Text.HTML.Scalpel
+import Data.Text
+import Text.HTML.Scalpel
+
+-- Data types
+
+data MathContent
+    = InlineMath String
+    | DisplayMath String
+
+-- Utilities
 
 scraper :: [Char] -> IO (Maybe Text)
 scraper url = scrapeURL url mainContent
     where
         mainContent :: Scraper Text Text
-        mainContent = text ("div" @: [hasClass "entry-content"])
+        mainContent =  ("div" @: [hasClass "entry-content"]) mathContents
+
+        mathContents :: Scraper Text MathContent
+        mathContents = chroots () inlineMath <|> displayMath
+
+        mathContent :: Scraper Text
+        
+        inlineMath :: Scraper Text InlineMath
+        inlineMath = do
+            src     <- 
+
+        displayMath :: Scraper Text DisplayMath
