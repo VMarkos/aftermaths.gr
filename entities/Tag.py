ac7a8284988d5752941bf27c3e8652eaaeee4f9d
bill <vassileiosmarkos@gmail.com>
2026-01-01 19:49:43 +0200

    Add figure tag parser

--- entities/Tag.py
+++ entities/Tag.py
@@ -6,15 +6,17 @@ from bs4.element import NavigableString, Tag
 
 class Tag:
     def __init__(self, bs_tag) -> None:
-        self.FLAT_TAGS = {"h2", "h3", "h4"}
+        self.FLAT_TAGS = {'h2', 'h3', 'h4'}
         tag_name = bs_tag.name
         match tag_name:
             case w if w in self.FLAT_TAGS:
                 self.__init_flat_tag(bs_tag)
-            case "blockquote":
+            case 'blockquote':
                 ...
-            case "p":
+            case 'p':
                 self.__init_p_tag(bs_tag)
+            case 'div':
+                self.__init_div(bs_tag)
             case _:
                 raise ValueError(f'Invalid tag name: "{tag_name}".')
 
@@ -24,6 +26,15 @@ class Tag:
         self.name = bs_tag.name
         self.text = bs_tag.text
         self.attrs = dict()
+
+    def __init_div_tag(bs_tag) -> None:
+        div_class = bs_tag.get('class')
+        match div_class:
+            case 'wp-block-image':
+                fig_tag = bs_tag.find('figure')
+                self.__init_figure(fig_tag)
+            case _:
+                raise ValueError(f'Unknown div class: "div_class"')
 
     def __init_p_tag(self, p_tag) -> None:
         p_class = p_tag.get("class")
@@ -36,7 +47,7 @@ class Tag:
             self.__init_display_math(p_tag)
         else:
             self.name = "p"
-            self.text = "".join(TagUtils.parse_tag_contents(t) for t in p_tag.contents)
+            self.text = TagUtils.parse_tag_text(p_tag)
             self.attrs = dict()
 
     def __init_display_math(self, p_tag) -> None:
@@ -45,8 +56,17 @@ class Tag:
         self.text = TagUtils.parse_display_math(img_tag)
         self.attrs = dict()
 
+    def __init_figure(self, fig_tag) -> None:
+        figure_url = fig_tag.get('data-orig-file')
+        figure_caption_element = fig_tag.find('figcaption', class_='wp-element-caption')
+        figure_caption = TagUtils.parse_tag_text(figure_caption_element)
+
 
 class TagUtils:
+    @staticmethod
+    def parse_tag_text(bs_tag) -> str:
+        return ''.join(parse_tag_contents(t) for t in bs_tag.contents)
+
     @staticmethod
     def parse_tag_contents(bs_tag) -> str:
         for c in bs_tag.contents:
@@ -67,7 +87,7 @@ class TagUtils:
                 return parse_inline_math(bs_tag)
             case "em":
                 return parse_emphasis_tag(bs_tag)
-            case "b":
+            case "b" | "strong":
                 return parse_bold_tag(bs_tag)
             case "i":
                 return parse_italics_tag(bs_tag)
