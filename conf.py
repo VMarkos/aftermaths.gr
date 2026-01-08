9dabc30fda22acd4cf14fbcf2d67a605c07e9d7f
bill <vassileiosmarkos@gmail.com>
2026-01-08 13:59:39 +0200

    Add half-baked main images

--- conf.py
+++ conf.py
@@ -17,7 +17,9 @@ release = '0.1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinxcontrib.youtube'
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
